e527afe40efa763af708e1ce00a5ad8dc81d720f
alex-samari <alexsamarim@gmail.com>
2019-01-10 12:30:09 -0500

    Added formatting using 'pipes'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
+  //  the component's CSS element selector
+  //  The CSS element selector, 'app-root', matches the name of the HTML element that identifies this component within a parent component's template.
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
