d983a5feef3a0e5476bab070b8d0ace0d6629393
Alex Samari <alisamarim7@gmail.com>
2019-01-15 16:05:02 -0500

    Made the HeroDetail component routable

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -7,12 +10,29 @@ import { Hero } from '../hero';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  //  The hero property must be an Input property, annotated with the @Input() decorator, 
-  //  because the external HeroesComponent will bind to it. See "heros.component.html"
+  // The hero property must be an Input property, annotated with the @Input() decorator, 
+  // because the external HeroesComponent will bind to it. See "heros.component.html"
   @Input() hero: Hero;
-  constructor() { }
+  constructor(
+    // The ActivatedRoute holds information about the route to this instance of the HeroDetailComponent.
+    // This component is interested in the route's bag of parameters extracted from the URL. The "id" parameter is the id of the hero to display.
+    private route: ActivatedRoute,
+    // The HeroService gets hero data from the remote server and this component will use it to get the hero-to-display.
+    private heroService: HeroService,
+    // The location is an Angular service for interacting with the browser. We'll use it later to navigate back to the view that navigated here.
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    this.getHero();
   }
 
+  // The route.snapshot is a static image of the route information shortly after the component was created.
+  // The paramMap is a dictionary of route parameter values extracted from the URL. The "id" key returns the id of the hero to fetch.
+  // Route parameters are always strings. The JavaScript (+) operator converts the string to a number, which is what a hero id should be.
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -22,4 +22,11 @@ export class HeroService {
     //  'of' is a method of the 'Observable' class
     return of(HEROES);
   }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    // backticks ( ` ) define a JavaScript template literal for embedding the 'id'.
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(HEROES.find(hero => hero.id === id));
+  }
 }
